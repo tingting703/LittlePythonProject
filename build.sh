19e75862c6add7989f776a884908821fce247ff6
tingting703 <tingting703@gmail.com>
2024-01-23 11:06:34 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
- #!/bin/sh
- 
+#!/bin/bash
+
+# Set environment variables
+export PYTHON3_PATH="/Library/Developer/CommandLineTools/usr/bin/python3"
+export PIP_PATH="/Library/Frameworks/Python.framework/Versions/3.12/bin/pip"
+export PYTEST_PATH="/Library/Frameworks/Python.framework/Versions/3.12/bin/pytest"
+
+# Add Python 3, pip, and pytest to the PATH
+export PATH="${PYTHON3_PATH}:${PIP_PATH}:${PYTEST_PATH}:${PATH}"
+
+# Display the versions for verification
+python3 --version
+pip --version
+pytest --version
+
+
 pip install -r requirements.txt
 cd shopping_cart/
 python3 -m pytest tests
